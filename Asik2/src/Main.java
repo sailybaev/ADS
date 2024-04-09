f16b7173fc8ab9d14c46b76f4cc2c63d782d3c01
sailybaev <sailybaevvv@gmail.com>
2024-04-09 21:25:35 +0500

    + Main.java
    with tests for all methods

--- Asik2/src/Main.java
+++ Asik2/src/Main.java
@@ -1,18 +1,72 @@
-import java.util.ArrayList;
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        ArrayList arr =new ArrayList<>();
+
+       // MyArrayList<Integer> myList = new MyArrayList<>();
+        MyLinkedList<Integer> myList = new MyLinkedList<>();
+
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        printArray(myList);
+
+        myList.set(1, 20);
+        printArray(myList);
+
+        myList.add(1, 10);
+        printArray(myList);
+
+        myList.addFirst(0);
+        printArray(myList);
+
+        myList.addLast(4);
+        printArray(myList);
+
+
+        System.out.println(myList.get(2));
+
+
+        System.out.println(myList.getFirst());
+
+
+        System.out.println(myList.getLast());
+
+
+        myList.remove(2);
+        printArray(myList);
+
+
+        myList.removeFirst();
+        printArray(myList);
+
+
+        myList.removeLast();
+        printArray(myList);
+
+
+        myList.sort();
+        printArray(myList);
+
+
+        System.out.println(myList.indexOf(20));
+
+
+        System.out.println(myList.lastIndexOf(20));
+
+
+        System.out.println(myList.exists(20));
+
+
+        myList.clear();
+        printArray(myList);
+
+
+        System.out.println(myList.size());
+    }
+    
+    public static void printArray(Iterable<Integer> arr) {
+        for (Object object : arr) {
+            System.out.print(object + " ");
+        }
+        System.out.println();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
---
+++ Asik2/src/NoSuchElementException.java
@@ -0,0 +1,2 @@
+public class NoSuchElementException extends Throwable {
+}
