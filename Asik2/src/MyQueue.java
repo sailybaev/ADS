b8f889bf587ab554355022aa56b6ce8f045eee98
sailybaev <sailybaevvv@gmail.com>
2024-04-16 12:38:18 +0500

    basic queue

---
+++ Asik2/src/MyQueue.java
@@ -0,0 +1,28 @@
+public class MyQueue<T> {
+    MyArrayList<T> queue = new MyArrayList<>();
+    public MyQueue() {
+    }
+    public T front() {
+        return queue.get(0);
+    }
+    public T back() {
+        return queue.get(queue.size() - 1);
+    }
+    public T dequeue() {
+        T item = queue.get(0);
+        queue.remove(0);
+        return item;
+    }
+    public void enqueue(T item) {
+        queue.add(item);
+    }
+    public boolean isEmpty() {
+        return queue.size() == 0;
+    }
+    public int size() {
+        return queue.size();
+    }
+    public void clear() {
+        queue.clear();
+    }
+}
